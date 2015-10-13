99cd352d3b70fab448f1f832a262eb3012a74827
Petri Hakola <petri.hakola@iki.fi>
2015-10-13 08:40:12 +0300

    Not quite ready.

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,22 +1,24 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (cond
+   (empty? a-seq) ""
+   :else (reduce str (map str (interpose " " a-seq)))))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (rest (reduce (fn [seq item] (conj seq x item)) [] a-seq)))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [a] (inc a)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn [seq item] (cons item seq)) [] a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (reduce (fn [[vmin vmax] val] [(min vmin val) (max vmax val)]) [100 -1] a-seq))
 
 (defn insert [sorted-seq n]
   [:-])
@@ -27,11 +29,12 @@
 (defn parity [a-seq]
   [:-])
 
-(defn minus [x]
-  :-)
+(defn minus 
+  ([x] (- x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params [& x]
+  (count x))
 
 (defn my-* [x]
   :-)
